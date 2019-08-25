b365877c00e71d8141977e610df4e27f04a1ecde
czoido <mrgalleta@gmail.com>
2019-08-25 15:05:20 +0200

    change example

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,190 +1,72 @@
-// ####################################################################################                                                             
-// This is the basic example to use the GoPiGo.
-// http://www.dexterindustries.com/GoPiGo/
-// History
-// ------------------------------------------------
-// Date                 Comments
-// 30 Aug 15            Initial Authoring
-// 02 Feb 16            Support Encoders
+/*
+ *  https://www.dexterindustries.com/GoPiGo3/
+ *  https://github.com/DexterInd/GoPiGo3
+ *
+ *  Copyright (c) 2017 Dexter Industries
+ *  Released under the MIT license (http://choosealicense.com/licenses/mit/).
+ *  For more information see https://github.com/DexterInd/GoPiGo3/blob/master/LICENSE.md
+ *
+ *  This code is an example for using GoPiGo3 motors.
+ *
+ *  Results:  When you run this program, you should see the encoder value for each motor. Manually rotate the left motor, and the right motor will follow.
+ *
+ *  Example compile command:
+ *    g++ -o leds leds.cpp ../GoPiGo3.cpp -I..
+ *  Example run command:
+ *    sudo ./leds
+ *
+ */
 
-// ## License
-//
-// GoPiGo for the Raspberry Pi: an open source robotics platform for the Raspberry Pi.
-// Copyright (C) 2017  Dexter Industries
+#include <GoPiGo3.h>   // for GoPiGo3
+#include <stdio.h>     // for printf
+#include <unistd.h>    // for usleep
+#include <signal.h>    // for catching exit signals
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+GoPiGo3 GPG;
 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+#define BRIGHTNESS_LIMIT 25
 
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+void exit_signal_handler(int signo);
 
-// ####################################################################################
+int main(){
+  signal(SIGINT, exit_signal_handler); // register the exit function for Ctrl+C
 
+  GPG.detect(); // Make sure that the GoPiGo3 is communicating and that the firmware is compatible with the drivers.
 
-#include <stdlib.h>
-#include <stdio.h>
-#include "gopigo-wrapper.h"
+  uint8_t i1 = 0;
+  uint8_t i2 = BRIGHTNESS_LIMIT / 3;
+  uint8_t i3 = (BRIGHTNESS_LIMIT * 2) / 3;
+  int8_t a1 = 1;
+  int8_t a2 = 1;
+  int8_t a3 = 1;
+  while(true){
+    GPG.set_led(LED_EYE_LEFT, i1, i2, i3);
+    GPG.set_led(LED_EYE_RIGHT, i2, i3, i1);
+    GPG.set_led(LED_BLINKER_LEFT, i1);
+    GPG.set_led(LED_BLINKER_RIGHT, i2);
 
-using namespace gopigo;
-
-int main(void)
-{
-    // Buffer for data being read/ written on the i2c bus
-    if(init()==-1)
-        exit(1);
-
-    led_on(1);
-
-    while(1)
-    {
-       char input[2];    /* Store 2 input characters */
-
-       printf("Enter cmd: ");
-       scanf(" %s", &input);
-
-       switch (input[0])
-       {
-            case 'w':
-                fwd();
-                break;
-
-            case 'a':
-                left();
-                break;
-
-            case 'd':
-                right();
-                break;
-
-            case 's':
-                bwd();
-                break;
-
-            case 'e':
-                increase_speed();
-                break;
-
-            case 'g':
-                decrease_speed();
-                break;
-
-            case 'z':
-                exit(0);
-                break;
-
-            case 'x':
-                stop();
-                break;
-
-            case 'i':
-                motor_fwd();
-                break;
-
-            case 'k':
-                motor_bwd();
-                break;
-
-            case 'n':
-                left_rot();
-                break;
-
-            case 'm':
-                right_rot();
-                break;
-
-            case 'l':
-                switch(input[1])
-                {
-                    case 'o':
-                        int led_val;
-                        printf("Enter 1 for left led and 0 for right led:\n");
-                        scanf(" %d", &led_val);
-                        led_on(led_val);
-                        break;
-                    case 'f':
-                        int led_val2;
-                        printf("Enter 1 for left led and 0 for right led:\n");
-                        scanf(" %d", &led_val2);
-                        led_off(led_val2);
-                        break;
-                    default:
-                        printf("invalid led command... (o for ON, f for OFF)\n");
-                        break;
-                }
-                break;
-
-            case 'r':
-                int servo_val;
-                printf("Enter servo position:\n");
-                scanf(" %d", &servo_val);
-                servo(servo_val);
-                break;
-
-            case 'v':
-                printf("Firmware Version: %d\n",fw_ver());
-                break; 
-                
-            case 'u':
-                printf("Ultrasonic Dist: %d\n",us_dist(15));
-                break;
-                    
-            case 't':
-                switch(input[1])
-                {
-                    case 'r':
-                        {
-                            int val = trim_read();
-                            if(val == -3){
-                            printf("-3, Trim Value Not set\n");
-                            }
-                            else{
-                            printf("%d\n", val-100);
-                            }
-                            break;
-                        }
-                    case 'w':
-                        {
-                            int val2;
-                            printf("Enter trim value to write to EEPROM(-100 to 100):\n");
-                            scanf(" %d", &val2);
-                            trim_write(val2);
-                            pi_sleep(100);
-                            printf("Value in EEPROM: %d\n", trim_read()-100);
-                            break;
-                        }
-                    case 't':
-                        {
-                            int val3;
-                            printf("Enter trim value to test(-100 to 100):\n");
-                            scanf(" %d", &val3);
-                            trim_test(val3);
-                            pi_sleep(100);
-                            printf("Value in EEPROM: %d\n", trim_read()-100);
-                            break;
-                        }
-                    default:
-                        printf("invalid trim command...\n");
-                        break;
-                   }
-                break;
-
-            case 'c':
-                printf("Motor 0 (left) travelled %i cm\n", enc_read(0));
-                printf("Motor 1 (right) travelled %i cm\n", enc_read(1));
-                break;
-
-            default:
-                printf("invalid command...\n");
-                break;
-       }
-
+    i1 += a1;
+    if(i1 == BRIGHTNESS_LIMIT || i1 == 0){
+      a1 *= -1;
     }
-    return 0;
+    i2 += a2;
+    if(i2 == BRIGHTNESS_LIMIT || i2 == 0){
+      a2 *= -1;
+    }
+    i3 += a3;
+    if(i3 == BRIGHTNESS_LIMIT || i3 == 0){
+      a3 *= -1;
+    }
+
+    // slow down
+    usleep(25000);
+  }
+}
+
+// Signal handler that will be called when Ctrl+C is pressed to stop the program
+void exit_signal_handler(int signo){
+  if(signo == SIGINT){
+    GPG.reset_all();    // Reset everything so there are no run-away motors
+    exit(-2);
+  }
 }
