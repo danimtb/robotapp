645465d8c4cda8ff2c7c775d68d2032d1f1b04cd
czoido <mrgalleta@gmail.com>
2019-09-24 08:08:39 +0300

    change ip 2

--- app/src/param_reader.cpp
+++ app/src/param_reader.cpp
@@ -7,6 +7,7 @@ RobotCarParams::RobotCarParams(int argc, char *argv[]): Kp(0.030),
                                                         max_val(80), 
                                                         uri("ws://192.168.0.51:8088")
 {
+    
     //./bin/robotapp 0.030 0.0005 0.025 100 http://169.254.223.91 8088
     if (argc >= 5)
     {
